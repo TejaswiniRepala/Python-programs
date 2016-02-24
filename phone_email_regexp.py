e94e42317c0b06fdef22b40f195769cbd647dc5a
TejaswiniRepala <repala.tejaswini@gmail.com>
2016-02-23 16:49:56 -0800

    Update phone_email_regexp.py

--- phone_email_regexp.py
+++ phone_email_regexp.py
@@ -1,7 +1,7 @@
 import re
 
 a = ''' Teja:408-657-4135,
-        Nani:408-332-1445 '''
+        Nani:408-363-1233 '''
 
 b='''tejaswini.repala@gmail.com
         repal.lalitha@gmail.com'''
@@ -9,7 +9,7 @@ b='''tejaswini.repala@gmail.com
 name=re.findall(r'[A-Z][a-z]*',a)
 phonenum=re.findall(r'\d{1,3}\-\d{1,3}\-\d{1,4}',a)
 #e-mail
-email = re.findall(r'([a-z0-9]+\.[a-z]+\@[a-z]+\.[a-z]+)',b)
+email = re.findall(r'(\w+\.\w+\@\w+\.\w+)',b)
 
 print(phonenum)
 print(name)
